cc1d549194e40ac205d58f48cd07b26794af63a0
Eduard Maura <edu.maura.puig@gmail.com>
2017-03-21 13:15:29 +0100

    Refactor divisible by ten condition into a method

--- src/main/java/CreditCardValidator.java
+++ src/main/java/CreditCardValidator.java
@@ -4,6 +4,10 @@
 public class CreditCardValidator {
     public boolean isLuhnValid(String creditCardNumber) {
         int number = Integer.parseInt(creditCardNumber);
+        return isTenDivisible(number);
+    }
+
+    private boolean isTenDivisible(int number) {
         return number % 10 == 0;
     }
 }
