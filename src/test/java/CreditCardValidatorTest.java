0ad02927091fda85d5c95ade9a018f81ba824d87
Eduard Maura <edu.maura.puig@gmail.com>
2017-03-23 11:54:56 +0100

    Add more test with three digit and long numbers

--- src/test/java/CreditCardValidatorTest.java
+++ src/test/java/CreditCardValidatorTest.java
@@ -27,6 +27,33 @@ public class CreditCardValidatorTest {
         assertIsLuhnValid(true, "18");
     }
 
+    @Test
+    public void nineOneIsValid() {
+        assertIsLuhnValid(true, "91");
+    }
+
+    @Test
+    public void sixOneTwoIsValid() {
+        assertIsLuhnValid(true, "612");
+    }
+
+    @Test
+    public void oneTwoThreeIsNotValid() {
+        assertIsLuhnValid(false, "123");
+    }
+
+    @Test
+    public void someLongNumberAreValid() {
+        assertIsLuhnValid(true, "49927398716");
+        assertIsLuhnValid(true, "79927398713");
+    }
+
+    @Test
+    public void someLongNumberAreNotValid() {
+        assertIsLuhnValid(false, "49927398712");
+        assertIsLuhnValid(false, "79927398715");
+    }
+
     private void assertIsLuhnValid(Boolean expected, String creditCardNumber) {
         CreditCardValidator validator = new CreditCardValidator();
         boolean isValid = validator.isLuhnValid(creditCardNumber);
